4b1c2a8c76f6e18699d377a73ccf808d8004cb17
gisselecardozo <46029683+gisselecardozo@users.noreply.github.com>
2021-03-24 14:57:32 -0300

    Update Tface.js

--- src/classes/Tface.js
+++ src/classes/Tface.js
@@ -1,10 +1,13 @@
 'use strict'
+
 // Classe Tface
 // Representa visualmente o face
 
 //three
 import * as THREE from 'three'
-import {Tponto} from './Tponto'
+import { Object3D } from 'three/src/core/Object3D.js'
+import { Vector3 } from 'three/src/math/Vector3';
+
 
 
 	/**
@@ -13,7 +16,7 @@ import {Tponto} from './Tponto'
    * @param arrFaces = []
 	*/
 
-class Tface extends Tponto {
+class Tface extends Object3D {
   constructor( nome, arrTponto, arrFaces ) { //Parametros: string, Vector3(X,Y,Z)
 
 		super();
@@ -25,43 +28,47 @@ class Tface extends Tponto {
     this.arrPt = arrTponto;
     //receives array of points name that makes up the face
     this.arrFaces = arrFaces
-}
+    
+  }
+/* recebe o array de pontos e o array com o nomes dos pontos de cada face, compara ambos 
+e separa os pontos de cada face determina a normal de cada face, cria a malha da mesma e adiciona na cena.
+*/
 
-criaFace(){
   //intersect array of point names with Tponto array (vector3)
-  console.log('passou1')
+ criaFace(){
   this.points=[]
-  console.log(this.points,this.arrPt,this.arrFaces)
   this.arrFaces.forEach((elemento) => {
-    console.log(elemento)
-      this.arrPt.forEach((e)=>{
-        if(e.nome == elemento){
-          this.points.push(this.arrPt)
+          this.arrPt.forEach((e)=>{
+           if(e.nome == elemento){
+            this.pontV = new THREE.Vector3(e.X,e.Y,e.Z)
+            this.points.push(this.pontV)
         }
       })
+      
 })
-console.log(this.points)
+
   //determines the normal of the faces
   this.triangulo = new THREE.Triangle(this.points[2], this.points[1], this.points[0])
   this.normal = new THREE.Vector3();
   this.triangulo.getNormal(this.normal);
-  //console.log(triangulo)
-  this.baseNormal = new THREE.Vector3(0, 0, 1);
+ 
+  this.baseNormal = new Vector3(0,0,1);
+  
+  this.q = new THREE.Quaternion().setFromUnitVectors(this.normal, this.baseNormal);
   
 
-  this.quaternion = new THREE.Quaternion().setFromUnitVectors(this.normal, this.baseNormal);
-  
-
+  //aplica a normal aos pontos 
   this.tempPoints = [];
   this.points.forEach(p => {
-  this.tempPoints.push(p.clone().applyQuaternion(quaternion));
+  this.tempPoints.push(p.clone().applyQuaternion(this.q));
+  
   })
 
 
   //generates the face mesh
-  this.shape = new THREE.Shape(tempPoints);
-  this.shapeGeom = new THREE.ShapeGeometry(shape);
-  this.mesh = new THREE.Mesh(shapeGeom, new THREE.MeshBasicMaterial({
+  this.shape = new THREE.Shape(this.tempPoints);
+  this.shapeGeom = new THREE.ShapeGeometry(this.shape);
+  this.mesh = new THREE.Mesh(this.shapeGeom, new THREE.MeshBasicMaterial({
    color: 0xfcee2b,
     side: THREE.DoubleSide,
      transparent: true,
@@ -69,12 +76,37 @@ console.log(this.points)
      wireframe: false
    }));
 
-     this.mesh.geometry.vertices = points;
+     this.mesh.geometry.vertices = this.points;
      this.name = String
      this.mesh.name
+
      //add face to the scene
-    this.add(mesh);
+ 
+    this.add(this.mesh);
+
+    //cria as arestas do solido
+    this.geom = new THREE.BufferGeometry().setFromPoints(this.points);
+    this.matLines = new THREE.LineBasicMaterial({color: "black"});
+    this.lines = new THREE.LineLoop(this.geom, this.matLines);
+    this.lines.name = "line"+ String(this.name);
+    this.add(this.lines);
+  
+    
+
+   }
+dispose() {//libera a memória
+  this.children.forEach(element => {
+    element.material.dispose();
+    element.geometry.dispose();
+  });
+  this.children = [];
 }
-}
+update( nome, pt ) {//atualiza o ponto: nome e posição
+  this.nome = nome;
+  this.children.forEach(element => {
+    element.position.copy(pt);
+  });
+}}
+
 
 export { Tface };
